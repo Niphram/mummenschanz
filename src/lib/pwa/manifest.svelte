697f15fd12fb115f6d8dc9eaad2000c6e93b93b8
Niphram <theniphram@gmail.com>
2024-12-27 17:39:25 +0100

    Revert "fully qualified urls"
    This reverts commit 4ecac3f2f5d3d4fbdca99065ef57052a9a821f21.

--- src/lib/pwa/manifest.svelte
+++ src/lib/pwa/manifest.svelte
@@ -1,38 +1,15 @@
 <script lang="ts">
-	import { page } from '$app/stores';
 	import { listCharacters } from '$lib/character-store';
 	import type { VersionedCharacter } from '$lib/systems';
 
-	const STATIC_MANIFEST = {
-		name: 'Mummenschanz',
-		short_name: 'Mummenschanz',
-		description: 'Your digital multi-system charactersheet',
-		theme_color: '#DBCA9A',
-		background_color: '#ECE3CA',
-		display: 'standalone',
-		scope: `${$page.url.origin}/`,
-		start_url: `${$page.url.origin}/`,
-		id: 'mummenschanz',
-		icons: [
-			{
-				src: `${$page.url.origin}/icons/512.png`,
-				type: 'image/png',
-				sizes: '512x512',
-			},
-			{
-				src: `${$page.url.origin}/icons/192.png`,
-				type: 'image/png',
-				sizes: '192x192',
-			},
-		],
-	};
+	import STATIC_MANIFEST from './manifest.json';
 
 	let characterList = $state(listCharacters());
 
 	function createManifest(characters: VersionedCharacter[]) {
 		const shortcuts = characters.slice(0, 3).map((c) => ({
 			name: c.name,
-			url: `${$page.url.origin}/character?id=${c.id}`,
+			url: `/character?id=${c.id}`,
 		}));
 
 		const combinedManifest = { ...STATIC_MANIFEST, shortcuts };
